3f070b5345313f7066de4d8bfddb2c4593180039
Narryss <144238002+Narryss@users.noreply.github.com>
2023-09-11 10:51:08 +0300

    Update 30.pas

--- 30.pas
+++ 30.pas
@@ -1,16 +1,22 @@
 var x,a,b,i:integer;
-begin
-for x:=1 to 100 do begin
-a:=x div 10;
-b:=x mod 10;
-if a=10 then write('C') else
-if a<4 then for i:=1 to a do write('X') else
-if a=4 then write('XL') else if a<=8 then begin write('L'); for i:=1 to a mod 5 do write('X'); end 
-else if a=9 then write('XC');
-
-if b=9 then write('IX') else if b =4 then write('IV')
-       else if b div 5 = 1 
-         then begin write('V'); for i:=1 to b mod 5 do write('I'); end else for i:=1 to b do write('I');
-         writeln('     ',x)
-         end;
+begin 
+  for x:=1 to 100 do
+    begin
+    a:=x div 10;
+  b:= x mod 10;
+  case a of
+    10: write('C');
+    1..3: for i:=1 to a do write('X');
+    4: write('XL');
+    5..8: begin write('L'); for i:=1 to a mod 5 do write('X'); end;
+    9: write('XC');
+  end;
+  case b of
+    9: write('IX');
+    4: write('IV');
+    1..3: for i:=1 to b do write('I');
+    5..8: begin write('V'); for i:=1 to b mod 5 do write('I'); end;
+  end;
+  writeln('   ',x);
+  end;
 end.
